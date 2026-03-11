385fcd4597f41908a9e0d8bc3bc2ac1ad78f4f8a
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-03-11 11:59:44 +0900

    refac: 同期するためだけの関数を追加

--- src/main.py
+++ src/main.py
@@ -1,19 +1,34 @@
 import os
+import sys
 from dotenv import load_dotenv
 import discord
 from discord import app_commands
 
 load_dotenv()
 
+
+class MeshibanClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self) -> None:
+        if "--sync-only" in sys.argv:
+            print("同期するために起動します")
+            await self.tree.sync()
+            print("同期しました。終了します。")
+            await self.close()
+            sys.exit()
+
+
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+client = MeshibanClient(intents=intents)
+tree = client.tree
 
 
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
-    await tree.sync()
 
 
 @tree.command(name="ping", description="ping! pong!")
